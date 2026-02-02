57b2ab2b34cf957e178455183d3227f7d210f24a
Josemar Neves <nevesjosemar9@gmail.com>
2026-02-02 13:14:36 +0100

    Novo commit: ajustes finais

--- res/wsgi.py
+++ res/wsgi.py
@@ -10,14 +10,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'res.settings')
 
-application = get_wsgi_application()
 
-from whitenoise import WhiteNoise
-from myproject.wsgi import application
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
