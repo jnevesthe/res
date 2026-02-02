bcb167851e9755aead1e5260e59c23ac609a7675
jnevesthe <nevesjosemar9@gmail.com>
2026-02-02 18:34:01 +0100

    Primeiro commit do projeto

--- res/wsgi.py
+++ res/wsgi.py
@@ -1,23 +1,21 @@
 """
-WSGI config for res project.
+WSGI config for codehub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'res.settings')
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
