b277a602eb4cc778eef1c06a1058ee18c8526456
jnevesthe <nevesjosemar9@gmail.com>
2026-02-02 20:18:56 +0100

    Update urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -25,7 +25,7 @@ urlpatterns=[
 
 # Serve arquivos estáticos e mídia no DEBUG (Termux)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
     
